08f81da95d88794a1bd6154505596e9111315228
Fran Martin <fmartinrivas2@gmail.com>
2021-04-07 14:50:08 +0200

    ADDED navigation drawer

--- app/src/main/java/com/example/ambulate/ui/main/MainActivity.java
+++ app/src/main/java/com/example/ambulate/ui/main/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.ambulate.ui.main;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.ambulate.R;
 
@@ -13,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    public void onGroupItemClick(MenuItem item) {
+        // TODO
     }
 
 }
